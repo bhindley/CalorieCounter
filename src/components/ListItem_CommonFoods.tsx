7bd9448cd60934dd193f5b189c856d168d2b3338
Benjamin Hindley <=>
2024-03-20 18:29:12 +0000

    removing redundant code

--- src/components/ListItem_CommonFoods.tsx
+++ src/components/ListItem_CommonFoods.tsx
@@ -1,19 +1,12 @@
 import React, {useState, useEffect} from "react";
 import {Text, View} from "react-native";
-import * as db from "../services/DBManager.js";
+import * as db from "../services/DBManager.ts";
 
 export default function ListItem_CommonFoods(props: {foodId: number}) {
-	const [foodData, setFoodData] = useState<any>(null);
-
-	useEffect(() => {
-		db.selectFoodById(props.foodId).then(data => {
-			setFoodData(data);
-		});
-	}, [props.foodId]);
 
 	return (
 		<View>
-			<Text>{foodData.name}</Text>
+
 		</View>
 	);
 }
