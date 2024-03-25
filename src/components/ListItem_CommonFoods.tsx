e3fb1baf41c5074099604eee62fd2bee3764591f
Benjamin Hindley <=>
2024-03-25 19:06:36 +0000

    refactoring

--- src/components/ListItem_CommonFoods.tsx
+++ src/components/ListItem_CommonFoods.tsx
@@ -1,8 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {Text, View} from "react-native";
-import * as db from "../services/DBManager.ts";
 
-export default function ListItem_CommonFoods(props: {foodId: number}) {
+export default function ListItem_CommonFoods(props: {foodId: number}): React.JSX.Element {
 
 	return (
 		<View>
