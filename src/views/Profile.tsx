0837a77ac38ff0a00a2c902812cb4b0b52d1535b
Ridwan <ridwan04@icloud.com>
2024-04-15 19:02:35 +0100

    Added buttons to profile page

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import * as Styles  from '../Styles';
+
 
 export default function Profile(): React.JSX.Element {
+  const [showBox, setShowBox] = useState<boolean>(false);
     return (
+        
         <View>
-            <Text>Profile</Text>
+      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
+        <Text style = {Styles.TextStyles.title}>Reminders and Notifications</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
+        <Text style = {Styles.TextStyles.title}>Units</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
+        <Text style = {Styles.TextStyles.title}>User Details</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
+        <Text style = {Styles.TextStyles.title}>Weight Goals</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
+        <Text style = {Styles.TextStyles.title}>Display & Accessibility</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
+        <Text style = {Styles.TextStyles.title}>Statistics & Trends</Text>
+      </TouchableOpacity>
+      
         </View>
     )
+
 }
