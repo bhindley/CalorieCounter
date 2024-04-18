7e0555f7795c6c5592451dbb8631e2dce64135df
Ridwan <ridwan04@icloud.com>
2024-04-18 16:03:54 +0100

    formatted page

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,38 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import * as Styles  from '../Styles';
-
+import * as styles from '../Styles'
 
 export default function Profile(): React.JSX.Element {
-  const [showBox, setShowBox] = useState<boolean>(false);
-    return (
-        
-        <View>
-      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
-        <Text style = {Styles.TextStyles.title}>Reminders and Notifications</Text>
+  return (
+    <View style={{ flex: 1,}}>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style = {styles.TextStyles.subtitle}>Reminders & Notifications</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
-        <Text style = {Styles.TextStyles.title}>Units</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style = {styles.TextStyles.subtitle}>Units</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
-        <Text style = {Styles.TextStyles.title}>User Details</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style = {styles.TextStyles.subtitle}>User Details</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
-        <Text style = {Styles.TextStyles.title}>Weight Goals</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style = {styles.TextStyles.subtitle}>Weight Goals</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
-        <Text style = {Styles.TextStyles.title}>Display & Accessibility</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style = {styles.TextStyles.subtitle}>Display & Accessibility</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => setShowBox(!showBox)}>
-        <Text style = {Styles.TextStyles.title}>Statistics & Trends</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text style = {styles.TextStyles.subtitle}>Statistics & Trends</Text>
       </TouchableOpacity>
-      
-        </View>
-    )
-
+    </View>
+  );
 }
+
+
