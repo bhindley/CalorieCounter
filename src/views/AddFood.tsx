32abcf0508fdaa46d4b69466343adc4c2364bb3f
Benjamin Hindley <=>
2024-04-18 17:36:50 +0100

    formatting

--- src/views/AddFood.tsx
+++ src/views/AddFood.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import {Text, View} from "react-native";
 
 export default function AddFood(): React.JSX.Element {
-    return (
-        <View>
-            <Text>AddFood</Text>
-        </View>
-    )
+	return (
+		<View>
+			<Text>AddFood</Text>
+		</View>
+	);
 }
--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import * as styles from '../Styles'
+import React from "react";
+import {Text, View, TouchableOpacity} from "react-native";
+import * as styles from "../Styles";
 
 export default function Profile(): React.JSX.Element {
-  return (
-    <View style={{ flex: 1,}}>
-      <TouchableOpacity onPress={() => {}}>
-        <Text style = {styles.TextStyles.subtitle}>Reminders & Notifications</Text>
-      </TouchableOpacity>
+	return (
+		<View style={{flex: 1}}>
+			<TouchableOpacity onPress={() => {}}>
+				<Text style={styles.TextStyles.subtitle}>Reminders & Notifications</Text>
+			</TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Text style = {styles.TextStyles.subtitle}>Units</Text>
-      </TouchableOpacity>
+			<TouchableOpacity onPress={() => {}}>
+				<Text style={styles.TextStyles.subtitle}>Units</Text>
+			</TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Text style = {styles.TextStyles.subtitle}>User Details</Text>
-      </TouchableOpacity>
+			<TouchableOpacity onPress={() => {}}>
+				<Text style={styles.TextStyles.subtitle}>User Details</Text>
+			</TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Text style = {styles.TextStyles.subtitle}>Weight Goals</Text>
-      </TouchableOpacity>
+			<TouchableOpacity onPress={() => {}}>
+				<Text style={styles.TextStyles.subtitle}>Weight Goals</Text>
+			</TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Text style = {styles.TextStyles.subtitle}>Display & Accessibility</Text>
-      </TouchableOpacity>
+			<TouchableOpacity onPress={() => {}}>
+				<Text style={styles.TextStyles.subtitle}>Display & Accessibility</Text>
+			</TouchableOpacity>
 
-      <TouchableOpacity onPress={() => {}}>
-        <Text style = {styles.TextStyles.subtitle}>Statistics & Trends</Text>
-      </TouchableOpacity>
-    </View>
-  );
+			<TouchableOpacity onPress={() => {}}>
+				<Text style={styles.TextStyles.subtitle}>Statistics & Trends</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
-
-
