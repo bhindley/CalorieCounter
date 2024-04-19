27e9335e5aca4188de083313bce044f8d3726f9e
Benjamin Hindley <22499573@stu.mmu.ac.uk>
2024-04-19 12:02:44 +0100

    main

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {Text, View, TouchableOpacity} from "react-native";
+import {NavigationAction} from "@react-navigation/native"
 
-export default function Home(): React.JSX.Element {
+export default function Home({ navigation }): React.JSX.Element {
 	return (
 		<View>
-			<Text>Home</Text>
+			<TouchableOpacity onPress={navigation.push("Add Workout")}>
+				<Text>Add Workout</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={navigation.push("Add Food")}>
+				<Text>Add Meal</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={navigation.push("Profile")}>
+				<Text>View Profile</Text>
+			</TouchableOpacity>
+
+			<TouchableOpacity onPress={navigation.push("Statistics")}>
+				<Text>View Trends</Text>
+			</TouchableOpacity>
 		</View>
 	);
 }
